8ab7cc1cdcdb2431dfa3fa04106e18469e36b08e
monjurctg <monjurpy.06@gmail.com>
2022-08-24 17:21:17 +0600

    qr add

--- src/Components/Qr.jsx
+++ src/Components/Qr.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {QRCodeCanvas} from 'qrcode.react';
 function Qr() {
   return (
-    <div>Qr</div>
+    <div className='qr-container'>
+      <h5>THE QR CODE:</h5>
+    <div className="qr">
+    <QRCodeCanvas value="https://reactjs.org/" style={{height:"200px",width:"200px"}} />
+    </div>
+
+       <p>or, Our Wallet Address</p>
+       <h4>bc1qpmgq4655pme90pa54r7gs3enuqgqpuysqcu50a</h4>
+
+    </div>
   )
 }
 
