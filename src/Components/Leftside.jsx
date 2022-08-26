0c6406b8e35a470082cedd638053829eaa1589b6
Anaada <anaada2015@gmail.com>
2022-08-26 14:44:31 +0600

    page done

---
+++ src/Components/Leftside.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import profile from "../assets/img/kia.jpg";
+
+function Leftside() {
+  return (
+    <div className="left-side">
+      <div className="left-description ">
+        <h4>Double Wall Stainless Steel</h4>
+        <h4>Bottle Vacuum Insulated -</h4>
+        <h4>20oz/500ml-High Quality-New</h4>
+        <h4>$20</h4>
+        <div className="ship">
+          <i class="fa-solid fa-ship"></i>
+          <p>Ships for $7.89</p>
+        </div>
+
+        <div className="posting-time ">
+          <p>Posted 2 days ago in Los Angeles, CA</p>
+          <p>Condition:New</p>
+          <p>Sports & Outside-Exercise-Fitness accessories</p>
+        </div>
+        <button className="place-order-btn text-center my-3">Buy Now</button>
+        <button className="ask-btn text-center">Ask</button>
+
+        <div className="d-flex my-3 justify-content-around">
+          <div className="l-btn ">
+            <i class="fa-regular fa-heart"></i>
+            <p>Save</p>
+          </div>
+
+          <div className="l-btn">
+            <i class="fa-solid fa-share-nodes"></i>
+            <p>Share</p>
+          </div>
+        </div>
+      </div>
+      <div className="left-profile">
+        <img src={profile} alt="" />
+
+        <div className="profile-description">
+          <h4>A-Tr</h4>
+          <p>Member since MAY 2022</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Leftside;
--- src/Pages/ProductDetailsPage.jsx
+++ src/Pages/ProductDetailsPage.jsx
@@ -1,21 +1,78 @@
-import React from 'react'
-import Header from '../Components/Shared/Header'
+import React from "react";
+import cars from "../assets/img/cars.jpg";
+import kia from "../assets/img/kia.jpg";
+import Leftside from "../Components/Leftside";
+
+import Header from "../Components/Shared/Header";
 
 function ProductDetailsPage() {
   return (
     <>
-    <Header/>
+      <Header />
 
-    <div className="container">
-    <div className="row">
-        <div className="col-md-8">left</div>
-        <div className="col-md-4">Roght</div>
-    </div>
-    </div>
+      <div className="container mt-2">
+        <div className="links">
+          <div className="d-flex ">
+            <p>Home</p>
+            <span>&gt;</span>
+          </div>
+          <div className="d-flex ">
+            <p>Sports & Outsides</p>
+            <span>&gt;</span>
+          </div>
+          <div className="d-flex ">
+            <p>Exercise</p>
+            <span>&gt;</span>
+          </div>
+          <div className="d-flex ">
+            <p>Filter & Accesories</p>
+            <span>&gt;</span>
+          </div>
+          <div className="d-flex ">
+            <p>Double wall Stainless Steel</p>
+            <span>&gt;</span>
+          </div>
+        </div>
+        <div className="description row">
+          <div className="col-8">
+            <div className="right-side">
+              <div className="big-image">
+                <img src={cars} alt="" />
+              </div>
+              <div className="small-images">
+                <img src={cars} className="active" alt="" />
+                <img src={kia} alt="" />
+                <img src={kia} alt="" />
+                <img src={kia} alt="" />
+              </div>
+              <div className="description-option">
+                <h4>Description</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
 
-      
+              <div className="buttons d-flex my-3">
+                <div className="love-btn ">
+                  <i class="fa-regular fa-heart"></i>
+                  <p>Save</p>
+                </div>
+                <div className="love-btn">
+                  <i class="fa-regular fa-flag"></i>
+                  <p>Report</p>
+                </div>
+                <div className="love-btn">
+                  <i class="fa-regular fa-square-share-nodes"></i>
+                  <p>Share</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <Leftside />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
