d9e9d6c650ff6872f4a1a1af62d7c0731afc6690
monjurctg <monjurpy.06@gmail.com>
2022-09-03 19:30:01 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-
-import {
-  BrowserRouter, Route, Routes
-} from "react-router-dom";
-import './App.css';
-import Checkout from './Pages/Checkout';
-import ConfirmPurchase from './Pages/ConfirmPurchase';
-import Home from './Pages/Home/Home';
-import ProductDetailsPage from './Pages/ProductDetailsPage';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Checkout from "./Pages/Checkout";
+import ConfirmPurchase from "./Pages/ConfirmPurchase";
+import Home from "./Pages/Home/Home";
+import ProductDetailsPage from "./Pages/ProductDetailsPage";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-{/* <Route path='/' element={<ProductDetailsPage/>}/> */}
-<Route path='/:id/:vehicle/:name' element={<ProductDetailsPage/>}/>
+      <Routes>
+        {/* <Route path='/' element={<ProductDetailsPage/>}/> */}
+        <Route path="/:id/:vehicle/:name" element={<ProductDetailsPage />} />
 
-      <Route path="/congratulation/:id/:vehicle/:name" element={<Home />}/>
-      <Route path="/checkout/:id/:vehicle/:name" element={<Checkout />}/>
-      <Route path="/confirm-purchase/:id/:vehicle/:name" element={<ConfirmPurchase />}/>
-
-    </Routes>
-  </BrowserRouter>
+        <Route path="/congratulation/:id/:vehicle/:name" element={<Home />} />
+        <Route path="checkout/:id/:vehicle/:name" element={<Checkout />} />
+        <Route
+          path="/confirm-purchase/:id/:vehicle/:name"
+          element={<ConfirmPurchase />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
